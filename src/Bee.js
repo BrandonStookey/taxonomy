bc0b21bb537d7f7d78d38fccd02b5f308be2576a
Brandon Stookey <bwstookey@gmail.com>
2016-01-18 16:10:12 -0600

    Project Complete

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,22 @@
 var Bee = function() {
+	// Grub.call(age, 5);
+	//age : 1,
+	//color: 'yellow',
+	//Grub.call(this, food);
+	//Bee.prototype = Object.create(Grub.prototype);
+	// Bee.prototype.constructor = Bee;
+	// Bee.prototype
+
+	// obG j.coloruG= 'ye
+
+  Grub.call(this);
+  this.age=5;
+  this.color='yellow';
+  this.job = 'keep on growing';
+
 };
+
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
+
 
